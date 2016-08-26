67b4dca3df9b885383ea21eb091892238e117c16
ibrahim <me@ibrahimdraidia.com>
2016-08-26 11:30:15 +0200

    compile then install

--- windows/install.bat
+++ windows/install.bat
@@ -1,7 +1,11 @@
 @echo off
-REM Powershell.exe -executionpolicy remotesigned -File proxyDaemonService.ps1
+echo compiling...
+cl /Fe:"proxyDaemon.exe" *.c 
 echo Copying file to data folder
-xcopy proxyDaemon.exe %APPDATA%"\Microsoft\Windows\Start Menu\Programs\Startup\"
+xcopy /y proxyDaemon.exe %APPDATA%"\Microsoft\Windows\Start Menu\Programs\Startup\"
 echo Starting Proxy Daemon
 start proxyDaemon.exe
-
+echo clean up
+del *.obj
+REM I am using VIM so ...
+del *.~
