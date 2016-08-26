99fe203fbaf27a1946f8b27c340ed4090de20d92
ibrahim <me@ibrahimdraidia.com>
2016-08-26 11:52:11 +0200

    added some change to install.bat

--- dist/install.bat
+++ dist/install.bat
@@ -1,6 +1,9 @@
 @echo off
-echo Copying file to data folder
+title=Proxy Deamon Installation
+echo Copying file to Start Up folder
 xcopy /y proxyDaemon.exe %APPDATA%"\Microsoft\Windows\Start Menu\Programs\Startup\"
 echo Starting Proxy Daemon
 start proxyDaemon.exe
 echo Installation Success !
+
+pause
